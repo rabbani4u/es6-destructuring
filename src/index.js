9d1f45c9a47dd8f98cf8946b96899429285204ef
rabbani4u <rabbani.iu31@gmail.com>
2022-12-04 12:17:11 +0600

    Initialize commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import animals from "./data";
+import data from "./data";
+
+const [first, second] = animals;
+
+console.log(second);
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
